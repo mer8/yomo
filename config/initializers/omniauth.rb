019baf07f5d8977cfcf13d348b24416693049eef
edwinhermawan <edwin@signaturepages.com>
2013-11-08 16:56:14 -0800

    Google Auth now authorizes Youtube Analytics

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :google_oauth2, '434092699375.apps.googleusercontent.com', 'or1NmEWn2QOmObdok9No6jcV', 
-		{name: "google_login", approval_prompt: ''}
+	provider :google_oauth2, 
+	'434092699375.apps.googleusercontent.com','or1NmEWn2QOmObdok9No6jcV', 
+		{scope: 'userinfo.email, userinfo.profile, https://www.googleapis.com/auth/yt-analytics.readonly', name: "google_login", approval_prompt: ''}
 end
