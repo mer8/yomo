ffd5696c7f1ab525cf4d2818e910ecd015183097
Chin-Chun Tseng <kevin074@gmail.com>
2013-11-21 16:00:28 -0800

    Dear Jesus

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -59,7 +59,7 @@ class SessionsController < ApplicationController
       :help=>false, :ids=>"channel==UCHjzmXcM52GFvKQxsSEuZ-g"}
 
     # Initialize the client, Youtube, and Youtube Analytics
-    client = Google::APIClient.new
+    client = Google::APIClient.new({:application_name => "testdevelop",:application_version => "1.0"})
     youtube = client.discovered_api(application_name:'youtube', application_version:'v3')
     youtube_analytics = client.discovered_api('youtubeAnalytics', 'v1')   
     # youtube = client.discovered_api(@YOUTUBE_API_SERVICE_NAME, @YOUTUBE_API_VERSION)
